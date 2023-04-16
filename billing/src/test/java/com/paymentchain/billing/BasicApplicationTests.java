4ae9ca59eeb9d9a5405fbfe2b1b412db069447c9
chemaff2 <chemaff@hotmail.com>
2023-04-16 13:27:21 +0200

    add test modification 3

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -3,6 +3,7 @@ package com.paymentchain.billing;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.postgresql.translation.messages_bg;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,7 +13,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "default message cambio test devops 2";
+ 	    String meessage= "default message cambio test devops 3";
  	    Assert.assertNotNull(meessage);
  }
  
