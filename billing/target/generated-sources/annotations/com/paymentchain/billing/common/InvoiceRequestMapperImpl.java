5dc51eee68b5caec604fa0215d65e1116f4d62a5
chemaff2 <chemaff@hotmail.com>
2023-04-17 10:42:56 +0200

    add integration test using mockito junit sping framework test

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -4,13 +4,13 @@ import com.paymentchain.billing.dto.InvoiceRequest;
 import com.paymentchain.billing.entities.Invoice;
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.processing.Generated;
+import javax.annotation.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-28T13:11:18+0200",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.5 (Oracle Corporation)"
+    date = "2021-03-07T14:54:33+0100",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_212 (Oracle Corporation)"
 )
 @Component
 public class InvoiceRequestMapperImpl implements InvoiceRequestMapper {
--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
@@ -4,13 +4,13 @@ import com.paymentchain.billing.dto.InvoiceResponse;
 import com.paymentchain.billing.entities.Invoice;
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.processing.Generated;
+import javax.annotation.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-28T13:11:18+0200",
-    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.5 (Oracle Corporation)"
+    date = "2021-03-07T14:54:33+0100",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_212 (Oracle Corporation)"
 )
 @Component
 public class InvoiceResposeMapperImpl implements InvoiceResposeMapper {
@@ -23,8 +23,8 @@ public class InvoiceResposeMapperImpl implements InvoiceResposeMapper {
 
         InvoiceResponse invoiceResponse = new InvoiceResponse();
 
-        invoiceResponse.setCustomer( source.getCustomerId() );
         invoiceResponse.setInvoiceId( source.getId() );
+        invoiceResponse.setCustomer( source.getCustomerId() );
         invoiceResponse.setNumber( source.getNumber() );
         invoiceResponse.setDetail( source.getDetail() );
         invoiceResponse.setAmount( source.getAmount() );
